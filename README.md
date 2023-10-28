1d382259c2cf72674b883ff92a66ab1986ad95d9
CarolineBellaLane <carolinelane@live.co.uk>
2023-10-28 14:53:49 +0100

    completes README, adds generatePassword function, completes challenge

--- README.md
+++ README.md
@@ -1,38 +1,73 @@
 # random-password-generator
 An app to generate random passwords.
 
-# console-finances
-
-This project is designed to solve the questions set out in the Module 5 assignment. It primarily uses JavaScript.  
+This project is designed to solve the challenges set out in the Module 5 assignment. It primarily uses JavaScript.  
 
 ## Description
 
-The project 
+This project is an application that generates a password for the user upon request based upon user preferences. 
 
-Please find a screenshot below of the  
+Please find a screenshot below of the landing area;
 
-![Screenshot of landing area](images/Screenshot-console-finances.png)
+![Screenshot of landing area](assets/images/Screenshot%20random-password-generator.png)
 
+The image shows the first prompt box the user encounters upon clicking the 'generate password' button. 
 
-It aims to show you my current skills in using the JavaScript that we have learned on the course so far, solving the requirements of the Module 05 Challenge in the EdX Frontend Web Development Bootcamp. 
+This project aims to show my current skills in using JavaScript that we have learned on the course so far, solving the requirements of the Module 05 Challenge in the EdX Frontend Web Development Bootcamp. 
 
-My biggest challenge was solving 
+My biggest challenge was understanding all the parts of the code that are present and whet each part does. 
 
- I have completed a JavaScript course previously, but I had not encointered some of the syntax required. 
+I have completed a JavaScript course previously, but I had not encointered some of the syntax required. 
 
 ## Usage
 
 Please find a link to the live application here; 
 
+https://github.com/cazbella/random-password-generator/settings/pages
 
+Please click the 'Generate Password' button to begin.
 
-Please open the HTML file in the browser and inspect the page. Then please view the console. You should see the following as the solution to the assignment; 
+1. A prompt should appear asking the user to "Please enter a number between 8-128 for your password length."
 
-![Screenshot of console](images/Screenshot-console.png)
+2. When the user enters a number of the required length, the process of selecting the character types the user requires in their password begins. If the user enters letters the following message appears - "Please enter a number between 8 and 128 characters. Please start again."
+If the user enters a number of the wrong size, the following message appears -
+"Incorrect number - Please enter a number between 8-128. Please start again." 
 
-The JavaScript file that logs this code contains comments. The tutor was asked if this was good practice and the advice given was that as I am new to web development, there should be lots of comments at this stage. Hopefully the comments and the commits explain the process I followed to complete the assignment. I have also made notes here to help me remember the process and what the syntax means. 
+3. The user is then guided through a series of confirms where they must click "cancel" or "OK" to select the type of characters that they would like to include in their password. If the user does not select any character types (i.e. clicks cancel on each confirm box), they are shown an alert which states - "You must select at least one character type for your password."
 
-I have chosen not to recursively recall the function on line 31. I feel that for the user this can get confusing and can feel like you're in an alert spiral. Once they are told about their error they have to re-start the process. The user hasn't gone too far through the process at this point sop hopefully wouldn't feel too frustrated. The alert informs them of the required action. 
+4. Once all the choices have been made by the user, a password of the desired length and characters should appear in the box, as shown below; 
+
+![Screenshot of completed process](assets/images/Screenshot-completed-process.png)
+
+If the user clicks the button at this point the process begins again. 
+
+The Console
+_______________
+It is a requirement that there are no errors in the console.
+
+Please open the HTML file in the browser and inspect the page. Then please view the console. You should see the following based on the choices made using the prompt/confirm boxes; 
+
+![Screenshot of console](assets/images/Screenshot-of-console.png)
+
+As you can see, there are no errors in the console. 
+
+A Note about Pseudocode
+_____________________________
+
+The JavaScript file that logs this code contains lots of comments. The tutor was asked if this was good practice and the advice given was that as I am new to web development, there should be lots of comments at this stage. Hopefully the comments and the commits explain the process I followed to complete the assignment. I have also made notes here to help me remember the process and what the syntax means. 
+
+A Note about Function
+_________________________
+
+I have chosen not to recursively recall the function on line 31. I feel that for the user prompts without a break can get confusing and can feel like you're in an alert spiral. Once they are told about their error they have to re-start the process. The user hasn't gone too far through the process at this point so hopefully wouldn't feel too frustrated. The alert informs them of the required action. 
+
+A Note about Responsiveness
+___________________________
+The assignment criteria states that the application should be responsive. I have tested this using chrome inspect and also on mobile. Please find a screenshot below showing the page on my phone; 
+
+![Screenshot of mobile phone](assets/images/mobile-screenshot.jpeg)
+
+The application appears to be responsive, although I have not personally written code to ensure this is the case. 
 
 ## References
 
--- assets/script.js
+++ assets/script.js
@@ -29,10 +29,10 @@ function getPasswordOptions() {
     return;
 
     //I could recursively call it here but I think that makes it annoying. they have to press button to start again. 
-    
+
   }
 
-  //this asks them to enter a number if they have antered a word/letters.
+  //this asks them to enter a number if they have entered a word/letters.
 
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Incorrect number - Please enter a number between 8-128. Please start again.")
@@ -40,7 +40,7 @@ function getPasswordOptions() {
   }
 
   //This checks if the length is correct
-
+  //below are the confirm boxes to collect user preferences
 
   var upperCaseCriteria = confirm("Do you require upper case letters?")
   console.log(upperCaseCriteria);
@@ -60,7 +60,7 @@ function getPasswordOptions() {
     alert("You must select at least one character type for your password.");
     return;
   }
-//makes sure user has at least one type of character selected. 
+  //makes sure user has at least one type of character selected. 
   var passwordOptions = {
     length: passwordLength,
     useUpperCase: upperCaseCriteria,
@@ -72,15 +72,7 @@ function getPasswordOptions() {
   //This object is used to store the user's preferences for generating a password. e.g. length: This property stores the length that the user added.  It's set to the value of passwordLength, which is obtained from the user's input.
 
   return passwordOptions;
-  //return "password"
-  //Math.Random()
-  //Math.floor()
 }
-
-
-
-//from mdn docs The .concat method in JavaScript is used to combine two or more arrays, creating a new array that contains the elements of the original arrays. It doesn't modify the existing arrays but returns a new array that's a result of the concatenation.
-
 
 // Function for getting a random element from an array
 
@@ -88,7 +80,6 @@ function getRandom(arr) {
   var randomIndex = Math.floor(Math.random() * arr.length);
   return arr[randomIndex];
 }
-
 //This creates a function to pick a random character from an array
 
 // function called getRandom. This function takes one argument, which is an array (arr). 
@@ -100,21 +91,74 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 // use conditionals to choose which arrays to concatenate
-  // for example, if they want the special characters and the lowercase characters,
-  //    put those arrays together
-function generatePassword() {
-  
+// for example, if they want the special characters and the lowercase characters,
+//    put those arrays together
+function generatePassword(options) {
+  // Define character arrays based on user input
 
+  var characterArrays = [];
+
+  if (options.useUpperCase) //here 'if' checks the condition in ()
+  {
+    //executes this if condition is truthy.
+    characterArrays = characterArrays.concat(upperCasedCharacters);
+    //this variable is being used to store combined character arrays.
+  }
+  if (options.useLowerCase) {
+    characterArrays = characterArrays.concat(lowerCasedCharacters);
+  }
+  if (options.useSpecialChars) {
+    characterArrays = characterArrays.concat(specialCharacters);
+  }
+  if (options.useNumbers) {
+    characterArrays = characterArrays.concat(numericCharacters);
+  }
+ 
+  //from mdn docs The .concat method in JavaScript is used to combine two or more arrays, creating a new array that contains the elements of the original arrays. It doesn't modify the existing arrays but returns a new array that's a result of the concatenation.
+  //With .concat(), create a new array by concatenating the elements of the selected character arrays. Adds the selected array to existing array so the characters can be used in the password. 
+
+  // Check if at least one character type is selected. The .length method returns the number of characters in the array. If it is strictly equal to 0 the array is empty.
+  if (characterArrays.length === 0) {
+    return "You must select at least one character type for your password.";
+    //this return checkout also returns a string message.
+  }
+
+  // Generate the password
+  var password = "";
+  for (var i = 0; i < options.length; i++) {
+    // Randomly select a character array from the concatenated arrays. For loop iterates over the array.
+    var selectedArray = getRandom(characterArrays);
+    // In each iteration of the loop, a random character array is selected from the characterArrays array. The getRandom function is used to choose a random character array. This array contains the character types chosen.
+    var randomCharacter = getRandom(selectedArray);
+    // Add the random character to the password. Within the loop, a random character is selected from the previously chosen selectedArray. The getRandom function is used again to randomly select a character from the chosen character array.
+    password += randomCharacter;
+  }
+
+  return password;
 }
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 generateBtn.addEventListener("click", function () {
-  var getPassword = getPasswordOptions();
-  console.log(getPassword)
-  var textAreaElement = document.querySelector("#password");
-  console.log(textAreaElement)
-  textAreaElement.textContent = getPassword
-})
+  var passwordOptions = getPasswordOptions(); // Changed variable name here
+  if (passwordOptions) {
+    var generatedPassword = generatePassword(passwordOptions); // Changed variable name here
+    var textAreaElement = document.querySelector("#password");
+    textAreaElement.textContent = generatedPassword;
+  }
+});
+
+
 //this enables button to be clicked and access to presenting the password in the box.
+
+
+// generateBtn.addEventListener("click", function () { ... });: This line adds an event listener to the "click" event of the generateBtn element. When the button is clicked, the function inside the addEventListener is executed.
+// var passwordOptions = getPasswordOptions();: This line calls the getPasswordOptions function and stores the returned value in the passwordOptions variable. This function prompts the user for various options related to the password generation, such as the length and character types.
+// if (passwordOptions) { ... }: This if statement checks if the passwordOptions variable has a truthy value. If it does, it means the user has provided valid options, and the code inside the if block will be executed.
+// var generatedPassword = generatePassword(passwordOptions);: This calls the generatePassword function with the passwordOptions object as an argument. The generatePassword function generates a password based on the options provided by the user and returns it.
+// var textAreaElement = document.querySelector("#password");: This selects the HTML element with the ID "password" using the querySelector method. This element is the text area where the generated password will be displayed.
+// textAreaElement.textContent = generatedPassword;: This sets the textContent property of the textAreaElement to the value of the generatedPassword. This displays the generated password in the text area on the web page.
+// Overall, this code block sets up an event listener for the button click, collects user input for password options, generates a password based on the user's choices, and then displays the generated password in the designated text area on the webpage.
